f07932fc7e9afbc19cae6ddb1dc0590f27163962
Ben Barton <99bbarton@gmail.com>
2024-01-23 13:49:33 +0100

    Added plot of eff vs masses for Z reco

--- Plotting/recoPlotter.py
+++ Plotting/recoPlotter.py
@@ -1,13 +1,18 @@
 
 
-from ROOT import TCanvas, TH1F, TFile, gStyle, TLegend, THStack, gPad
+from ROOT import TCanvas, TH1F, TFile, gStyle, TLegend, THStack, gPad, TGraph
 import os
 import sys
 import argparse
+from array import array
 
 sys.path.append("../Framework/")
 import Colors as cols
 
+
+# -----------------------------------------------------------------------------------------------------------------------------
+
+#Take in command line arguments and return a dictionary of settings to use for calculations/plotting
 def parseArgs():
     argparser = argparse.ArgumentParser(description="Script to plot GEN-level variables")
     argparser.add_argument("-i", "--inDir", required=True, action="store", help="A directory to find the input root files")
@@ -26,33 +31,65 @@ def parseArgs():
 
     return args
 
+# -----------------------------------------------------------------------------------------------------------------------------
+
+#Main driving function, calls calculation/plotting functions based on args
 def main(args):
     if args.decay == "Z":
-        calcRecoZEffs(args)
+        recoEffs_Z(args)
 
-def calcRecoZEffs(args):
+# -----------------------------------------------------------------------------------------------------------------------------
 
-    nElDecays = 0
-    elDecaysMatch = 0
-    nMuDecays = 0
-    muDecaysMatch = 0
-    nTauDecays = 0
-    tauDecaysMatch = 0
-    nHadDecays = 0
-    hadDecaysMatch = 0
-    elD1DecaysMatch = 0
-    muD1DecaysMatch = 0
-    tauD1DecaysMatch = 0
-    elD2DecaysMatch = 0
-    muD2DecaysMatch = 0
-    tauD2DecaysMatch = 0
-    hadDecaysMatch_dt = 0
-    hadDecaysMatch_pn = 0
-    nElCorrectReco = 0
-    nMuCorrectReco = 0
-    nTauCorrectReco = 0
+#Calculate the Z RECO efficiency for each decay mode using MCTruth info from GenProducerTauZ.py and RECO info from ZProducer.py
+def recoEffs_Z(args):
+
+    nElDecays = []
+    elDecaysMatch = []
+    nMuDecays = []
+    muDecaysMatch = []
+    nTauDecays = []
+    tauDecaysMatch = []
+    nHadDecays = []
+    hadDecaysMatch = []
+    elD1DecaysMatch = []
+    muD1DecaysMatch = []
+    tauD1DecaysMatch = []
+    elD2DecaysMatch = []
+    muD2DecaysMatch = []
+    tauD2DecaysMatch = []
+    hadDecaysMatch_dt = []
+    hadDecaysMatch_pn = []
+    nElCorrectReco = []
+    nMuCorrectReco = []
+    nTauCorrectReco = []
+
+    effs_ee = []
+    effs_mumu = []
+    effs_tautau = []
+    effs_had_pn = []
+    effs_had_dt = []
 
     for mass in args.masses:
+        nElDecays.append(0)
+        elDecaysMatch.append(0)
+        nMuDecays.append(0)
+        muDecaysMatch.append(0)
+        nTauDecays.append(0)
+        tauDecaysMatch.append(0)
+        nHadDecays.append(0)
+        hadDecaysMatch.append(0)
+        elD1DecaysMatch.append(0)
+        muD1DecaysMatch.append(0)
+        tauD1DecaysMatch.append(0)
+        elD2DecaysMatch.append(0)
+        muD2DecaysMatch.append(0)
+        tauD2DecaysMatch.append(0)
+        hadDecaysMatch_dt.append(0)
+        hadDecaysMatch_pn.append(0)
+        nElCorrectReco.append(0)
+        nMuCorrectReco.append(0)
+        nTauCorrectReco.append(0)
+
 
         for year in args.years:
             inFile = TFile.Open(args.inDir + "/taustarToTauZ_m"+mass+"_"+year+".root", "READ")
@@ -64,44 +101,51 @@ def calcRecoZEffs(args):
             #Do some efficiency calculations
             print("Parameters for Z reconstruction:")
 
-            nHadDecays += tree.GetEntries("Gen_zDM==0")
-            nElDecays += tree.GetEntries("Gen_zDM==1")
-            nMuDecays += tree.GetEntries("Gen_zDM==2")
-            nTauDecays += tree.GetEntries("Gen_zDM==3")
-            hadDecaysMatch += tree.GetEntries("Gen_zDM==0 && Z_dm==0")
-            elDecaysMatch += tree.GetEntries("Gen_zDM==1 && Z_dm==1")
-            muDecaysMatch += tree.GetEntries("Gen_zDM==2 && Z_dm==2")
-            tauDecaysMatch += tree.GetEntries("Gen_zDM==3 && Z_dm==3")
+            nHadDecays[-1] += tree.GetEntries("Gen_zDM==0")
+            nElDecays[-1] += tree.GetEntries("Gen_zDM==1")
+            nMuDecays[-1] += tree.GetEntries("Gen_zDM==2")
+            nTauDecays[-1] += tree.GetEntries("Gen_zDM==3")
+            hadDecaysMatch[-1] += tree.GetEntries("Gen_zDM==0 && Z_dm==0")
+            elDecaysMatch[-1] += tree.GetEntries("Gen_zDM==1 && Z_dm==1")
+            muDecaysMatch[-1] += tree.GetEntries("Gen_zDM==2 && Z_dm==2")
+            tauDecaysMatch[-1] += tree.GetEntries("Gen_zDM==3 && Z_dm==3")
 
-            hadDecaysMatch_pn += tree.GetEntries("Z_jetIdxPN > 0 && Gen_zDM==0 && Z_dm==0 && FatJet_genJetAK8Idx[Z_jetIdxPN]==Gen_zGenAK8Idx")
-            hadDecaysMatch_dt += tree.GetEntries("Z_jetIdxDT > 0 && Gen_zDM==0 && Z_dm==0 && FatJet_genJetAK8Idx[Z_jetIdxDT]==Gen_zGenAK8Idx")
+            hadDecaysMatch_pn[-1] += tree.GetEntries("Z_jetIdxPN > 0 && Gen_zDM==0 && Z_dm==0 && FatJet_genJetAK8Idx[Z_jetIdxPN]==Gen_zGenAK8Idx")
+            hadDecaysMatch_dt[-1] += tree.GetEntries("Z_jetIdxDT > 0 && Gen_zDM==0 && Z_dm==0 && FatJet_genJetAK8Idx[Z_jetIdxDT]==Gen_zGenAK8Idx")
 
-            elD1DecaysMatch += tree.GetEntries("Gen_zDM==1 && Z_dm==1 && Electron_genPartIdx[Z_d1Idx]==Gen_zDau1Idx")
-            muD1DecaysMatch += tree.GetEntries("Gen_zDM==2 && Z_dm==2 && Muon_genPartIdx[Z_d1Idx]==Gen_zDau1Idx")
-            tauD1DecaysMatch += tree.GetEntries("Gen_zDM==3 && Z_dm==3 && Tau_genPartIdx[Z_d1Idx]==Gen_zDau1Idx")
-            elD2DecaysMatch += tree.GetEntries("Gen_zDM==1 && Z_dm==1 && Electron_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
-            muD2DecaysMatch += tree.GetEntries("Gen_zDM==2 && Z_dm==2 && Muon_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
-            tauD2DecaysMatch += tree.GetEntries("Gen_zDM==3 && Z_dm==3 && Tau_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
+            elD1DecaysMatch[-1] += tree.GetEntries("Gen_zDM==1 && Z_dm==1 && Electron_genPartIdx[Z_d1Idx]==Gen_zDau1Idx")
+            muD1DecaysMatch[-1] += tree.GetEntries("Gen_zDM==2 && Z_dm==2 && Muon_genPartIdx[Z_d1Idx]==Gen_zDau1Idx")
+            tauD1DecaysMatch[-1] += tree.GetEntries("Gen_zDM==3 && Z_dm==3 && Tau_genPartIdx[Z_d1Idx]==Gen_zDau1Idx")
+            elD2DecaysMatch[-1] += tree.GetEntries("Gen_zDM==1 && Z_dm==1 && Electron_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
+            muD2DecaysMatch[-1] += tree.GetEntries("Gen_zDM==2 && Z_dm==2 && Muon_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
+            tauD2DecaysMatch[-1] += tree.GetEntries("Gen_zDM==3 && Z_dm==3 && Tau_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
 
-            nElCorrectReco +=  tree.GetEntries("Gen_zDM==1 && Z_dm==1 && Electron_genPartIdx[Z_d1Idx]==Gen_zDau1Idx && Electron_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
-            nMuCorrectReco += tree.GetEntries("Gen_zDM==2 && Z_dm==2 && Muon_genPartIdx[Z_d1Idx]==Gen_zDau1Idx && Muon_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
-            nTauCorrectReco += tree.GetEntries("Gen_zDM==3 && Z_dm==3 && Tau_genPartIdx[Z_d1Idx]==Gen_zDau1Idx && Tau_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
+            nElCorrectReco[-1] +=  tree.GetEntries("Gen_zDM==1 && Z_dm==1 && Electron_genPartIdx[Z_d1Idx]==Gen_zDau1Idx && Electron_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
+            nMuCorrectReco[-1] += tree.GetEntries("Gen_zDM==2 && Z_dm==2 && Muon_genPartIdx[Z_d1Idx]==Gen_zDau1Idx && Muon_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
+            nTauCorrectReco[-1] += tree.GetEntries("Gen_zDM==3 && Z_dm==3 && Tau_genPartIdx[Z_d1Idx]==Gen_zDau1Idx && Tau_genPartIdx[Z_d2Idx]==Gen_zDau2Idx")
 
+        effs_ee.append(float(nElCorrectReco[-1]) / nElDecays[-1] * 100.0)
+        effs_mumu.append(float(nMuCorrectReco[-1]) / nMuDecays[-1] * 100.0)
+        effs_tautau.append(nTauCorrectReco[-1] / nTauDecays[-1])
+        effs_had_pn.append(float(hadDecaysMatch_pn[-1]) / hadDecaysMatch[-1] * 100.0)
+        effs_had_dt.append(float(hadDecaysMatch_dt[-1]) / hadDecaysMatch[-1] * 100.0)
+
+    #Print out is done for masses * years cumulative
     print("\n=============  Z RECO parameters: =============")
     print("years = " + str(args.years))
-    print("masses = " + str(args.masses))
+    print("mass = " + str(args.massses))
     print("----------------- Z->ee -----------------------")
     print("Num Events (MC Truth): " + str(nElDecays))
-    print("Decay mode choice eff: {:.2f}%".format(float(elDecaysMatch) / nElDecays * 100.0))
-    print("Daughter 1 match eff:  {:.2f}%".format(float(elD1DecaysMatch) / elDecaysMatch * 100.0))
-    print("Daughter 2 match eff:  {:.2f}%".format(float(elD2DecaysMatch) / elDecaysMatch * 100.0))
-    print("Total Z->ee reco eff:  {:.2f}%".format(float(nElCorrectReco) / nElDecays * 100.0)) 
+    print("Decay mode choice eff: {:.2f}%".format(float(sum(elDecaysMatch)) / sum(nElDecays) * 100.0))
+    print("Daughter 1 match eff:  {:.2f}%".format(float(sum(elD1DecaysMatch)) / sum(elDecaysMatch) * 100.0))
+    print("Daughter 2 match eff:  {:.2f}%".format(float(sum(elD2DecaysMatch)) / sum(elDecaysMatch) * 100.0))
+    print("Total Z->ee reco eff:  {:.2f}%".format(float(sum(nElCorrectReco)) / sum(nElDecays) * 100.0)) 
     print("---------------- Z->mumu ----------------------")
     print("Num Events (MC Truth): " + str(nMuDecays))
-    print("Decay mode choice eff: {:.2f}%".format(float(muDecaysMatch) / nMuDecays * 100.0))
-    print("Daughter 1 match eff:  {:.2f}%".format(float(muD1DecaysMatch) / muDecaysMatch * 100.0))
-    print("Daughter 2 match eff:  {:.2f}%".format(float(muD2DecaysMatch) / muDecaysMatch * 100.0))
-    print("Total Z->mumu reco eff: {:.2f}%".format(float(nMuCorrectReco) / nMuDecays * 100.0)) 
+    print("Decay mode choice eff: {:.2f}%".format(float(sum(muDecaysMatch)) / sum(nMuDecays) * 100.0))
+    print("Daughter 1 match eff:  {:.2f}%".format(float(sum(muD1DecaysMatch)) / sum(muDecaysMatch) * 100.0))
+    print("Daughter 2 match eff:  {:.2f}%".format(float(sum(muD2DecaysMatch)) / sum(muDecaysMatch) * 100.0))
+    print("Total Z->mumu reco eff: {:.2f}%".format(float(sum(nMuCorrectReco)) / sum(nMuDecays) * 100.0)) 
     #print("--------------- Z->tautau ---------------------")
     #print("Num Events (MC Truth): {:.2f}%" + str(nTauDecays))
     #print("Decay mode choice eff: {:.2f}%" + str(tauDecaysMatch / nTauDecays))
@@ -110,11 +154,60 @@ def calcRecoZEffs(args):
     #print("Total Z->ee reco eff:  {:.2f}%" + str(nTauCorrectReco / nTauDecays)) 
     print("----------------- Z->jets -----------------------")
     print("Num Events (MC Truth): " + str(nHadDecays))
-    print("Decay mode choice eff: {:.2f}%".format(float(hadDecaysMatch) / nHadDecays * 100.0))
-    print("ParticleNet match eff:  {:.2f}%".format(float(hadDecaysMatch_pn) / hadDecaysMatch * 100.0))
-    print("DeepBoostedJet match eff:  {:.2f}%".format(float(hadDecaysMatch_dt) / hadDecaysMatch * 100.0))
+    print("Decay mode choice eff: {:.2f}%".format(float(sum(hadDecaysMatch)) / sum(nHadDecays) * 100.0))
+    print("ParticleNet match eff:  {:.2f}%".format(float(sum(hadDecaysMatch_pn)) / sum(hadDecaysMatch) * 100.0))
+    print("DeepBoostedJet match eff:  {:.2f}%".format(float(sum(hadDecaysMatch_dt)) / sum(hadDecaysMatch) * 100.0))
     
     print("=================================================\n")
+
+    #Now make plots
+    g_ee = TGraph(array("f", args.masses), array("f", effs_ee))
+    g_mumu = TGraph(array("f", args.masses), array("f", effs_mumu))
+    g_tautau = TGraph(array("f", args.masses), array("f", effs_tautau))
+    g_had_pn = TGraph(array("f", args.masses), array("f", effs_had_pn))
+    g_had_dt = TGraph(array("f", args.masses), array("f", effs_had_dt))
+
+    g_ee.SetMaximum(105)
+    g_mumu.SetMaximum(105)
+    g_tautau.SetMaximum(105)
+    g_had_pn.SetMaximum(105)
+    g_had_dt.SetMaximum(105)
+    g_ee.SetMinimum(0)
+    g_mumu.SetMinimum(0)
+    g_tautau.SetMinimum(0)
+    g_had_pn.SetMinimum(0)
+    g_had_dt.SetMinimum(0)
+
+    specTitle = ""
+    if len(args.years) == 1:
+        specTitle += " " + args.years[0]
+    g_ee.SetTitle("RECO Eff" + specTitle + ": Z#rightarrow ee;#tau* Mass [GeV]; Efficiency [%]")
+    g_mumu.SetTitle("RECO Eff" + specTitle + ": Z#rightarrow #mu#mu;#tau* Mass [GeV]; Efficiency [%]")
+    g_tautau.SetTitle("RECO Eff" + specTitle + ": Z#rightarrow#tau#tau;#tau* Mass [GeV]; Efficiency [%]")
+    g_had_pn.SetTitle("RECO Eff (PN ID)" + specTitle + ": Z#rightarrow had;#tau* Mass [GeV]; Efficiency [%]")
+    g_had_dt.SetTitle("RECO Eff (DT ID)" + specTitle + ": Z#rightarrow had;#tau* Mass [GeV]; Efficiency [%]")
+
+    gStyle.SetOptStat(0)
+    canv = TCanvas("zCanv", "Z RECO Plots", 1800, 1000)
+    canv.Divide(3,2)
+    canv.cd(1)
+    g_ee.Draw("ALP")
+    canv.cd(2)
+    g_mumu.Draw("ALP")
+    canv.cd(3)
+    g_tautau.Draw("ALP")
+    canv.cd(4)
+    g_had_pn.Draw("ALP")
+    canv.cd(5)
+    g_had_dt.Draw("ALP")
+
+    resp = raw_input("Hit ENTER to save and close plot... ")
+    canv.SaveAs("Plots/recoEffs_Z.png")
+
+
+    #END calRecoEffs_Z
+
+# -----------------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     args = parseArgs()
