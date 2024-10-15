5b1864d766c89ef32a2fa91b90202d9252fef5e4
Ben Barton <99bbarton@gmail.com>
2024-10-15 16:29:58 +0200

    Added channel support to plotting script

--- Plotting/plotter.py
+++ Plotting/plotter.py
@@ -18,44 +18,40 @@ from Cuts import getCuts
 
 # Map of variable options to [plotting string, histogram axis title string, nBins, bins min, bins max]
 varToPlotParams = { 
-    "SIG_M"   : ["", "#M_{#tau*} [GeV]", 12, 0, 5500],
-    "Z_PT"    : ["Z_pt", "Z_{pT} [GeV]", 100, 0, 1000],
-    "Z_ETA"   : ["Z_eta", "#eta_Z", 10, -2.5, 2.5],
-    "Z_DAUDR" : ["Z_dauDR", "#DeltaR(Z_d1, Z_d2)", 80, 0, 4.0],
-    "Z_M"     : ["Z_mass", "Reco Z Mass [GeV]", 30, 60, 120],
-    #"VIS_M"   : ["CH_visM", "Visible Mass [GeV]", 530, 200, 5500], #TODO need to add a per-channel plotting function, mayber just an if + 3x replaces?
-    #"MIN_COL_M"   : ["CH_minCollM", "Min Collinear Mass [GeV]", 530, 200, 5500] #TODO
-    #"MAX_COL_M"   : ["CH_maxCollM", "Max Collinear Mass [GeV]", 530, 200, 5500] #TODO
+    "SIG_M"     : ["", "#M_{#tau*} [GeV]", 12, 0, 5500],
+    "Z_PT"      : ["Z_pt", "Z_{pT} [GeV]", 100, 0, 1000],
+    "Z_ETA"     : ["Z_eta", "#eta_Z", 10, -2.5, 2.5],
+    "Z_DAUDR"   : ["Z_dauDR", "#DeltaR(Z_d1, Z_d2)", 80, 0, 4.0],
+    "Z_M"       : ["Z_mass", "Reco Z Mass [GeV]", 30, 60, 120],
+    "VIS_M"     : ["CHANNEL_visM", "Visible Mass [GeV]", 530, 200, 5500], 
+    "MIN_COL_M" : ["CHANNEL_minCollM", "Min Collinear Mass [GeV]", 212, 200, 5500], #25 GeV/bin default
+    "MAX_COL_M" : ["CHANNEL_maxCollM", "Max Collinear Mass [GeV]", 212, 200, 5500]
 }
 
 plotEachToLeg = {
     "PROC" : "Process",
     "YEAR" : "Year",
-    "NA": ""
+    "CH"   : "Channel",
+    "MASS" : "#tau^* Mass [GeV]",
+    "NA"   : ""
 }
 
 ## ------------------------------------------------------------------------------------------------------------------------------------------------- ##
 
 def parseArgs():
     argparser = argparse.ArgumentParser(description="Script to make various mass plots")
-    argparser.add_argument("vars", nargs='+', choices=["SIG_M", "Z_PT", "Z_ETA", "Z_DAUDR", "Z_M", "VIS_M", "COL_M"], help="What to plot. If one argument is provided, a 1D hist of that variable will be produced. If a second argument is also provided, the first arg will be plotted on the x-axis and the second, the y-axis and sim for 3 args.")
+    argparser.add_argument("vars", nargs='+', choices=["SIG_M", "Z_PT", "Z_ETA", "Z_DAUDR", "Z_M", "VIS_M", "MIN_COL_M", "MAX_COL_M"], help="What to plot. If one argument is provided, a 1D hist of that variable will be produced. If a second argument is also provided, the first arg will be plotted on the x-axis and the second, the y-axis and sim for 3 args.")
     argparser.add_argument("-i", "--inDir", required=True, action="store", help="A directory to find the input root files")
     argparser.add_argument("-y", "--years", required=True, action= "append", choices=["ALL", "2015","2016", "2017", "2018","RUN2", "2022post", "2022", "2023post", "2023", "RUN3"], help="Which year's data to plot")
     argparser.add_argument("-p", "--processes", required=True, type=str, choices = ["ALL", "SIG_ALL", "SIG_DEF", "M250","M500","M750","M1000","M1500","M2000","M2500","M3000","M3500","M4000","M4500","M5000"], action="append", help = "Which signal masses to plot. SIG_DEF=[M250, M1000, M3000, M5000]")
-    argparser.add_argument("-e","--plotEach", choices=["PROC", "YEAR"], default="NA", help="If specified, will make a hist per mass/proc/year rather than combining them into a single hist")
+    argparser.add_argument("-c", "--channel", action="append", choices=["ALL", "ETau", "MuTau", "TauTau"], default=["ALL"], help="What tau decay channels to use" )
+    argparser.add_argument("-e","--plotEach", choices=["PROC", "YEAR", "CH", "MASS"], default="NA", help="If specified, will make a hist per channel/proc/year rather than combining them into a single hist")
     argparser.add_argument("-d", "--dataTier", choices=["GEN", "RECO","GEN_RECO"], default="RECO",help="What data tier to use. If len(vars)==2, GEN_RECO will user var1:GEN and var2:reco")
     argparser.add_argument("--palette",choices=getPalettes(), default="line_cool", help="A palette to use for plotting")
     args = argparser.parse_args()  
 
-    #print("args.vars = " + str(args.vars) + " which has length " + str(len(args.vars)))
-    #if len(args.vars) != 1 or len(args.vars) != 2:
-    #    print("ERROR: Must provide one or two positional arguments to specify plotting. e.g python3 massPlotter.py Z_M Z_PT <optional args>")
-    #    exit(1)
     if len(args.vars) == 1 and args.vars[0] == "SIG_M":
         print("ERROR: SIG_M can only be plotted as the independent variable (with another variable)")
-        exit(1)
-    if "Z_ETA" in args.vars or "VIS_M" in args.vars or "COL_M" in args.vars:
-        print("ERROR: These variables are not yet supported: Z_ETA, VIS_M, COL_M")
         exit(1)
 
     if args.inDir.startswith("/store"):
@@ -75,7 +71,9 @@ def parseArgs():
     elif "SIG" in args.processes:
         args.processes = ["M250","M500","M750","M1000","M1500","M2000","M2500","M3000","M3500","M4000","M4500","M5000"]
 
-        
+    if "ALL" in args.channel:
+        args.channel = ["ETau", "MuTau", "TauTau"]
+
     return args
 
 ## ------------------------------------------------------------------------------------------------------------------------------------------------- ##
@@ -87,18 +85,14 @@ def main(args):
     if len(args.vars) == 1:
         plot1D(filelist, args)
     elif len(args.vars) == 2:
-        if args.plotEach == "NA":
-            plot2D_hists(filelist, args)
-        else:
-            plot2D_graphs(filelist, args)
-
+        plot2D_hists(filelist, args)
 
 ## ------------------------------------------------------------------------------------------------------------------------------------------------- ##
 
 def getFileList(args):
     filelist = {}
 
-    if args.plotEach == "NA":
+    if args.plotEach == "NA" or args.plotEach == "CH":
         filelist["ALL"] = []
         for proc in args.processes:
             for year in args.years:
@@ -128,6 +122,13 @@ def getFileList(args):
                 else:
                     filename = args.inDir + proc + "_"
                 filelist[proc].append(filename + year + ".root")
+    elif args.plotEach == "MASS": #Really just a special case of PROC above but convenient for e.g. legend making for sig only plotting
+        for proc in args.processes:
+            mass = proc[1:]
+            filelist[mass] = []
+            for year in args.years:
+                filename = args.inDir + "taustarToTauZ_m" + mass + "_"
+                filelist[mass].append(filename + year + ".root")
 
     return filelist
 ## ------------------------------------------------------------------------------------------------------------------------------------------------- ##
@@ -136,31 +137,50 @@ def plot1D(filelist, args):
     global varToPlotParams, plotEachToLeg
 
     canv = TCanvas("canv", "Mass Plotting", 800, 600)
-    makeLegend = len(filelist.keys()) > 1
+    makeLegend = args.plotEach != "NA"
     if makeLegend:
         gStyle.SetOptStat(0)
         leg = TLegend(0.7, 0.7, 0.9, 0.9, plotEachToLeg[args.plotEach])
 
     plotParams = varToPlotParams[args.vars[0]]
-    cutStr = getCuts(args.vars[0], args.dataTier)
 
     hists = []
     maxVal = 0
-    for hNum, hName in enumerate(filelist.keys()):
+
+    hNameList = []
+    if args.plotEach == "CH":
+        hNameList = args.channels
+        fileNames = filelist["ALL"]
+    else:
+        hNameList = filelist.keys()
+        fileNames = []  
+
+    for hNum, hName in enumerate(hNameList):
         hists.append(TH1F("h_"+hName, ";"+plotParams[1]+";Events", plotParams[2], plotParams[3], plotParams[4]))
         
-        for filename in filelist[hName]:
+        if args.plotEach == "NA":
+            fileNames = filelist.keys()[hNum]
+
+        for filename in fileNames:
             inFile = TFile.Open(filename, "r")
             if inFile == "None":
                 print("ERROR: Could not read file " + inFile)
                 continue
             tree = inFile.Get("Events")
-            hTemp = TH1F("h_"+hName+"_temp", ";"+plotParams[1]+";Events", plotParams[2], plotParams[3], plotParams[4])
+
+            for ch in args.channel:
+                if args.plotEach == "CH" and ch != hName:
+                    continue
+
+                cutStr = getCuts(args.vars[0], ch, args.dataTier)
+                hTemp = TH1F("h_"+hName+"_temp", ";"+plotParams[1]+";Events", plotParams[2], plotParams[3], plotParams[4])
             
-            tree.Draw(plotParams[0] + ">>+h_"+hName+"_temp", cutStr)
+                plotStr = plotParams[0].replace("CHANNEL", ch)
+                tree.Draw(plotStr + ">>+h_"+hName+"_temp", cutStr)
             
-            hists[-1].Add(hTemp)
-            #hTemp.Clear()
+                hists[-1].Add(hTemp)
+                del hTemp
+
             inFile.Close()
         
         hists[hNum].SetLineColor(getColor(args.palette, hNum))
@@ -201,42 +221,87 @@ def plot2D_hists(filelist, args):
     canv = TCanvas("canv", "Mass Plotting", 800, 600)
     gStyle.SetOptStat(0)
 
+    makeLegend = len(filelist.keys()) > 1 or args.plotEach == "CH"
+    if makeLegend:
+        gStyle.SetOptStat(0)
+        leg = TLegend(0.7, 0.7, 0.9, 0.9, plotEachToLeg[args.plotEach])
+
+
     plotParamsD1 = varToPlotParams[args.vars[0]]
-    cutStrD1 = getCuts(args.vars[0], args.dataTier.split("_")[0])
     plotParamsD2 = varToPlotParams[args.vars[1]]
-    
+
+    #Reduce to 5 GeV/bin for max_vs_min coll mass plots to avoid visible binning effects
+    if args.vars[1] == "MAX_COL_M" and args.vars[0] == "MIN_COL_M":
+        plotParamsD1[2] = (plotParamsD1[4] - plotParamsD1[3]) / 5 
+        plotParamsD2[2] = (plotParamsD2[4] - plotParamsD2[3]) / 5
+
     if args.dataTier == "GEN_RECO":
-        dataTier = "RECO"
+        dataTier1 = "GEN"
+        dataTier2 = "RECO"
     else:
-        dataTier = args.dataTier
-    cutStrD2 = getCuts(args.vars[1], dataTier)
+        dataTier1 = dataTier2 = args.dataTier
 
-    cutStr = "(" + cutStrD1 + ") && (" + cutStrD2 + ")"
+    hists = []
+    hNameList = []
+    if args.plotEach == "CH":
+        hNameList = args.channels
+        fileNames = filelist["ALL"]
+    else:
+        hNameList = filelist.keys()
+        fileNames = []  
 
-    hist = TH2F("h_"+args.vars[0] + "_vs_"+args.vars[1], ";"+plotParamsD1[1]+";"+plotParamsD2[1], plotParamsD1[2], plotParamsD1[3], plotParamsD1[4], plotParamsD2[2], plotParamsD2[3], plotParamsD2[4])
-    
-    for filename in filelist["ALL"]:
-        inFile = TFile.Open(filename, "r")
-        if inFile == "None":
-            print("ERROR: Could not read file " + inFile)
-            continue
-        tree = inFile.Get("Events")
-        hTemp = TH2F("h_temp_2d", ";"+plotParamsD1[1]+";"+plotParamsD2[1], plotParamsD1[2], plotParamsD1[3], plotParamsD1[4], plotParamsD2[2], plotParamsD2[3], plotParamsD2[4])
+    for hNum, hName in enumerate(hNameList):
+        hists.append(TH2F("h_"+args.vars[0] + "_vs_"+args.vars[1]+"_"+hName, ";"+plotParamsD1[1]+";"+plotParamsD2[1], plotParamsD1[2], plotParamsD1[3], plotParamsD1[4], plotParamsD2[2], plotParamsD2[3], plotParamsD2[4]))
         
-        tree.Draw(plotParamsD2[0] + ":" + plotParamsD1[0] + ">>+h_temp_2d", cutStr)
+        if args.plotEach == "NA":
+            fileNames = filelist.keys()[hNum]
 
-        hist.Add(hTemp)
-        #hTemp.Clear()
-        inFile.Close()
+        for filename in fileNames:
+            inFile = TFile.Open(filename, "r")
+            if inFile == "None":
+                print("ERROR: Could not read file " + inFile)
+                continue
+            tree = inFile.Get("Events")
 
-    canv.Clear()
-    hist.Draw("COLZ")
+            for ch in args.channel:
+                if args.plotEach == "CH" and ch != hName:
+                    continue
 
+                cutStrD1 = getCuts(args.vars[0], ch, dataTier1)
+                cutStrD2 = getCuts(args.vars[1], ch, dataTier2)
+                cutStr = "(" + cutStrD1 + ") && (" + cutStrD2 + ")"
+
+                hTemp = TH2F("h_temp_2d", ";"+plotParamsD1[1]+";"+plotParamsD2[1], plotParamsD1[2], plotParamsD1[3], plotParamsD1[4], plotParamsD2[2], plotParamsD2[3], plotParamsD2[4])
+
+                plotStr = plotParamsD2[0].replace("CHANNEL", ch) + ":" + plotParamsD1[0].replace("CHANNEL", ch)
+                tree.Draw(plotStr + ">>+h_temp_2d", cutStr)
+
+                hists[-1].add(hTemp)
+                del hTemp
+
+            inFile.Close()
+        
+        if args.plotEach != "NA":
+            hists[hNum].SetLineColor(getColor(args.palette, hNum))
+            hists[hNum].SetMarkerColor(getColor(args.palette, hNum))
+            drawStyle = ""
+            leg.AddEntry(hists[hNum], hName, "L")
+        else:
+            drawStyle = "COLZ"
+    
+    for hN, hist in enumerate(hists):
+        if hN == 0:
+            hist.Draw(drawStyle)
+        else:
+            hist.Draw(drawStyle + " SAME")
+    
+    if args.plotEach != "NA":
+        leg.Draw()
+    
     canv.Update()
     wait = input("Hit ENTER to save plot and end... ")
     plotname = "Plots/" + args.vars[0].lower() + "_vs_" + args.vars[1].lower()
     canv.SaveAs(plotname + ".png")
-
 
 ## ------------------------------------------------------------------------------------------------------------------------------------------------- ##
 
