09e3959725d0e39228d6236b2c62c032b1cb0267
BenBarton <bbarton@ucdavis.edu>
2025-01-17 16:19:30 -0600

    Updates to NanoAOD producers and plotter after 2D collinear mass plot investigation

--- Plotting/plotter.py
+++ Plotting/plotter.py
@@ -21,7 +21,7 @@ varToPlotParams = {
     "SIG_M"     : ["", "#M_{#tau*} [GeV]", 12, 0, 5500], #TODO
     "Z_PT"      : ["Z_pt", "Z_{pT} [GeV]", 60, 0, 3000],
     "Z_ETA"     : ["Z_eta", "#eta_Z", 10, -2.5, 2.5],
-    "Z_DAUDR"   : ["Z_dauDR", "#DeltaR(Z_d1, Z_d2)", 80, 0, 4.0],
+    "Z_DAUDR"   : ["Z_dauDR", "#DeltaR(Z_d1, Z_d2)", 20, 0, 1.0],
     "Z_M"       : ["Z_mass", "Reco Z Mass [GeV]", 60, 60, 120],
     "Z_DM"      : ["Z_dm", "Decay Mode of Z", 3, -0.5, 2.5],
     "Z_JETR"    : ["Z_jetR", "Best jet R", 3, 3, 9],
@@ -34,6 +34,9 @@ varToPlotParams = {
     "GEN_ZAK8IDX" : ["Gen_z_DATATIER_AK8Idx", "GEN Matched _DATATIER_ AK8 Idx", 5, -0.5, 5.5],
     "GEN_ZAK8_M" : ["GenJetAK8_mass[Gen_zGenAK8Idx]", "AK8 Jet Mass of GEN Particles [GeV]", 100, 0, 200],
     "GEN_ZAK4_M" : ["GenJet_mass[Gen_zGenAK4Idx]", "AK4 Jet Mass of Gen Particles [GeV]", 70, 0, 140],
+    "TAU_VISINVDR": ["Gen_tau_visInvDR", "#DeltaR(tau_vis, tau_inv)]", 10, 0, 0.2],
+    "TSTAU_VISINVDR": ["Gen_tsTau_visInvDR", "#DeltaR(tsTau_vis, tsTau_inv)]", 10, 0, 0.2],
+    "VISINVDR"      : [["Gen_tau_visInvDR", "Gen_tsTau_visInvDR"], "#DeltaR(#tau_vis, #tau_inv)]", 10, 0, 0.1],      
     #"Z_AK4M_GEN"    : ["Jet_mass[Gen_zRecAK4Idx]", "Rec AK4 Jet Mass of GEN-Matched Jet [GeV]", 30, 60, 120], # Redundant with Z_AK*M with cut requiring match
     #"Z_AK8M_GEN"    : ["FatJet_mass[Gen_zRecAK8Idx]", "Rec AK8 Jet Mass of GEN-Matched Jet [GeV]", 30, 60, 120],
     "Z_PN_SCORE": ["FatJet_particleNetWithMass_ZvsQCD[Z_jetIdxPN]", "Particle Net ZvsQCD Score", 20, 0.9, 1.0],
@@ -41,7 +44,8 @@ varToPlotParams = {
     "VIS_M"     : ["CHANNEL_visM", "Visible Mass [GeV]", 106, 200, 5500], 
     "MIN_COL_M" : ["CHANNEL_minCollM", "Min Collinear Mass [GeV]", 116, 200, 6000], #50 GeV/bin default
     "MAX_COL_M" : ["CHANNEL_maxCollM", "Max Collinear Mass [GeV]", 116, 200, 6000],
-    #    "DPHI"      : ["CHANNEL_CHANNELdPhi", "#Delta#phi", 63, 0, 6.3]  #TODO, update dPhi to not have second info, make it cos^@(dphi) maybe?
+    "COS2DPHI"  : ["CHANNEL_CHANNELCos2DPhi", "#Delta#phi", 63, 0, 6.3],
+    "TAUSDR"    : ["CHANNEL_CHANNELDR", "#DeltaR(#tau_{1},#tau_{2}))", 50, 0, 5]
 }
 
 # Map of variable options to either a list of values or a list of [min, max] "bin ranges"
@@ -74,13 +78,13 @@ def parseArgs():
     #argparser.add_argument("-g", "--graph", action="store_true", help="Requries 2 vars. If specified, will make a graph of the passed vars rather than a 2D hist" )
     argparser.add_argument("-d", "--dataTier", choices=["Gen", "Rec","Gen_Rec"], default="Rec",help="What data tier to use. If len(vars)==2, GEN_RECO will user var1:GEN and var2:reco")
     argparser.add_argument("-b", "--modifyBins", nargs='+', help="Modifying the binning of the produced hists. [1, 6] args allowed in order: nBinsD1, minBinD1, maxBinD1, nBinsD2, minBinD2, maxBinD2" )
-    argparser.add_argument("--effCut", type="str", )
     argparser.add_argument("-l", "--logScale", nargs="+", choices=["X","Y","Z"], help="Axis to make log scale")
     argparser.add_argument("-n", "--normalize", action="store_true", help="If specified, will normalize distributions to unit area (1D hists only)")
     argparser.add_argument("--cuts", type=str, help="Cuts to apply. Overrides default cuts" )
+    argparser.add_argument("-a", "--addCuts", type=str, help="A cut to add to those returned by Cuts::getCuts")
     argparser.add_argument("--effCut", type=str, help="If provided, will make an effiency plot by requiring the specified additional cut for the numerator and the cuts from '--cuts' for both numerator and denominator" )
     argparser.add_argument("--palette",choices=getPalettes(), default="line_cool", help="A palette to use for plotting")
-    argparser.add_argument("--drawStyle", help="A ROOT drawstyle to use for the plot.")
+    argparser.add_argument("--drawStyle", help="A ROOT drawstyle to use for the plot.'SAME' + multiple vars will plot all the vars on the same 1D hist")
     argparser.add_argument("--nS", action="store_true", help="If specified, will disabled the stat box on 1D hists")
     argparser.add_argument("--nP", action="store_true", help="If specified, will not prompt the user before saving and closing plots")
     argparser.add_argument("--save", action="append", choices = [".pdf", ".png", ".C", "ALL"], default=[], help="What file types to save plots as. Default not saved.")
@@ -98,10 +102,10 @@ def parseArgs():
     if len(args.vars) == 2 and args.normalize:
         print("WARNING: Normalization not currently supported for 2D plots")
     if len(args.vars) > 2:
-        print("WARNING: Only the first two arguments will be used!")
-    if len(args.vars) != 2 and args.graph:
-        print("ERROR: Graphs can only be made from 2 specified variables!")
-        exit(1)
+        print("WARNING: Only the first two arguments will be used unless '--drawStyle SAME' is passed!")
+    #if len(args.vars) != 2 and args.graph:
+    #    print("ERROR: Graphs can only be made from 2 specified variables!")
+    #    exit(1)
 
 
     if args.inDir.startswith("/store"):
@@ -167,13 +171,20 @@ def main(args):
 
     filelist = getFileList(args)
 
+    
     if len(args.vars) == 1:
         plot1D(filelist, args)
-    elif len(args.vars) == 2:
-        if args.graph:
-            plot2D_graph(filelist, args)
+    elif args.drawStyle:
+        if args.drawStyle == "SAME":
+            print("SAME feature not yet implemented!") #TODO
+            return 1
         else:
             plot2D_hists(filelist, args)
+    else:
+        #if args.graph:
+        #    plot2D_graph(filelist, args)
+        #else:
+        plot2D_hists(filelist, args)
 
 ## ------------------------------------------------------------------------------------------------------------------------------------------------- ##
 
@@ -234,7 +245,11 @@ def plot1D(filelist, args):
         leg = TLegend(0.7, 0.7, 0.9, 0.9, plotEachToLeg[args.plotEach])
 
     plotParams = varToPlotParams[args.vars[0]]
-    plotParams[0] = plotParams[0].replace("_DATATIER_", args.dataTier[0])
+    if type(plotParams[0]) is str:
+        plotParams[0] = plotParams[0].replace("_DATATIER_", args.dataTier[0])
+    elif type(plotParams[0]) is list:
+        for i, varVer in enumerate(plotParams[0]):
+            plotParams[0][i] = varVer.replace("_DATATIER_", args.dataTier[0])
     plotParams[1] = plotParams[1].replace("_DATATIER_", args.dataTier[0])
 
     titleStr = ";" + plotParams[1] + ";"
@@ -283,24 +298,45 @@ def plot1D(filelist, args):
                     cutStr = args.cuts
                 else:
                     cutStr = getCuts(args.vars[0], ch, args.dataTier)
-                cutStr = cutStr.replace("CHANNEL", ch)
+                    
                 if args.plotEach == "DM":
                     cutStr += " && Z_dm==" + str(dmFromName[hName])
+                if args.addCuts:
+                    cutStr += " && " + args.addCuts
+                cutStr = cutStr.replace("CHANNEL", ch)
 
                 hTemp = TH1F("h_"+hName+"_temp", titleStr, plotParams[2], plotParams[3], plotParams[4])
-            
-                plotStr = plotParams[0].replace("CHANNEL", ch)                    
-                tree.Draw(plotStr + ">>+h_"+hName+"_temp", cutStr)
 
-                hists[-1].Add(hTemp)
-                del hTemp
+                if type(plotParams[0]) is str:
+                    plotStr = plotParams[0].replace("CHANNEL", ch)                    
+                    tree.Draw(plotStr + ">>+h_"+hName+"_temp", cutStr)
 
-                if args.effCut:
-                    hTemp_num = TH1F("h_"+hName+"_temp_num", titleStr, plotParams[2], plotParams[3], plotParams[4])
-                    cutStr += " && " + args.effCut
-                    tree.Draw(plotStr + ">>+h_"+hName+"_temp_num", cutStr)
-                    numHists[-1].Add(hTemp_num)
-                    del hTemp_num
+                    hists[-1].Add(hTemp)
+                    del hTemp
+
+                    if args.effCut:
+                        hTemp_num = TH1F("h_"+hName+"_temp_num", titleStr, plotParams[2], plotParams[3], plotParams[4])
+                        cutStr += " && " + args.effCut
+                        tree.Draw(plotStr + ">>+h_"+hName+"_temp_num", cutStr)
+                        numHists[-1].Add(hTemp_num)
+                        del hTemp_num
+                elif type(plotParams[0] is list):
+                    for i, varVer in enumerate(plotParams[0]):
+                        if i > 0:
+                            hTemp = TH1F("h_"+hName+"_temp", titleStr, plotParams[2], plotParams[3], plotParams[4])
+                            
+                        plotStr = varVer.replace("CHANNEL", ch)
+                        tree.Draw(plotStr + ">>+h_"+hName+"_temp", cutStr)
+
+                        hists[-1].Add(hTemp)
+                        del hTemp
+
+                        if args.effCut:
+                            hTemp_num = TH1F("h_"+hName+"_temp_num", titleStr, plotParams[2], plotParams[3], plotParams[4])
+                            cutStr += " && " + args.effCut
+                            tree.Draw(plotStr + ">>+h_"+hName+"_temp_num", cutStr)
+                            numHists[-1].Add(hTemp_num)
+                            del hTemp_num
 
             inFile.Close()
         
@@ -327,14 +363,19 @@ def plot1D(filelist, args):
         if args.effCut:
             hist.Sumw2()
             numHists[hN].Sumw2()
-            hist.Divide(numHists[hN])
+            numHists[hN].Divide(hist)
             maxVal = 1.1
-
-        hist.SetMaximum(maxVal)
-        if hN == 0:
-            hist.Draw("HIST")
+            numHists[hN].SetMaximum(maxVal)
+            if  hN == 0:
+                numHists[hN].Draw("HIST")
+            else:
+                numHists[hN].Draw("HIST SAME")
         else:
-            hist.Draw("HIST SAME")
+            hist.SetMaximum(maxVal)
+            if hN == 0:
+                hist.Draw("HIST")
+            else:
+                hist.Draw("HIST SAME")
     
     if makeLegend:
         leg.Draw()
@@ -374,18 +415,14 @@ def plot2D_hists(filelist, args):
         gStyle.SetOptStat(0)
         leg = TLegend(0.7, 0.5, 0.9, 0.7, plotEachToLeg[args.plotEach])
 
-
     plotParamsD1 = varToPlotParams[args.vars[0]]
     plotParamsD2 = varToPlotParams[args.vars[1]]
-    if args.dataTier == "GEN_RECO":
-        dataTier1 = "GEN"
-        dataTier2 = "RECO"
-    else:
-        dataTier1 = dataTier2 = args.dataTier
-    plotParamsD1[0] = plotParamsD1[0].replace("_DATATIER_", dataTier1)
-    plotParamsD1[1] = plotParamsD1[1].replace("_DATATIER_", dataTier1)
-    plotParamsD2[0] = plotParamsD2[0].replace("_DATATIER_", dataTier2)
-    plotParamsD2[1] = plotParamsD2[1].replace("_DATATIER_", dataTier2)
+    
+    plotParamsD1[0] = plotParamsD1[0].replace("_DATATIER_", args.dataTier[0])
+    plotParamsD1[1] = plotParamsD1[1].replace("_DATATIER_", args.dataTier[0])
+    plotParamsD2[0] = plotParamsD2[0].replace("_DATATIER_", args.dataTier[1])
+    plotParamsD2[1] = plotParamsD2[1].replace("_DATATIER_", args.dataTier[1])
+
     
     #Reduce to 5 GeV/bin for max_vs_min coll mass plots to avoid visible binning effects
     if args.vars[1] == "MAX_COL_M" and args.vars[0] == "MIN_COL_M" and not args.modifyBins:
@@ -395,7 +432,7 @@ def plot2D_hists(filelist, args):
     hists = []
     hNameList = []
     if args.plotEach == "CH":
-        hNameList = args.channels
+        hNameList = args.channel
     elif args.plotEach == "DM":
         hNameList = ["ee", "mumu", "had"]
         dmFromName = {"ee" : 1, "mumu" : 2, "had" : 0}
@@ -425,12 +462,14 @@ def plot2D_hists(filelist, args):
                 if args.cuts:
                     cutStr = args.cuts
                 else:
-                    cutStrD1 = getCuts(args.vars[0], ch, dataTier1)
-                    cutStrD2 = getCuts(args.vars[1], ch, dataTier2)
+                    cutStrD1 = getCuts(args.vars[0], ch, args.dataTier[0])
+                    cutStrD2 = getCuts(args.vars[1], ch, args.dataTier[1])
                     cutStr = "(" + cutStrD1 + ") && (" + cutStrD2 + ")"
-                cutStr = cutStr.replace("CHANNEL", ch)
                 if args.plotEach == "DM":
                     cutStr += " && Z_dm==" + str(dmFromName[hName])
+                if args.addCuts:
+                    cutStr += " && " + args.addCuts
+                cutStr = cutStr.replace("CHANNEL", ch)
 
                 hTemp = TH2F("h_temp_2d", ";"+plotParamsD1[1]+";"+plotParamsD2[1], plotParamsD1[2], plotParamsD1[3], plotParamsD1[4], plotParamsD2[2], plotParamsD2[3], plotParamsD2[4])
 
@@ -512,15 +551,11 @@ def plot2D_graph(filelist, args):
     
     plotParamsD1 = varToPlotParams[args.vars[0]]
     plotParamsD2 = varToPlotParams[args.vars[1]]
-    if args.dataTier == "GEN_RECO":
-        dataTier1 = "GEN"
-        dataTier2 = "RECO"
-    else:
-        dataTier1 = dataTier2 = args.dataTier
-    plotParamsD1[0] = plotParamsD1[0].replace("_DATATIER_", dataTier1)
-    plotParamsD1[1] = plotParamsD1[1].replace("_DATATIER_", dataTier1)
-    plotParamsD2[0] = plotParamsD2[0].replace("_DATATIER_", dataTier2)
-    plotParamsD2[1] = plotParamsD2[1].replace("_DATATIER_", dataTier2)
+    
+    plotParamsD1[0] = plotParamsD1[0].replace("_DATATIER_", args.dataTier[0])
+    plotParamsD1[1] = plotParamsD1[1].replace("_DATATIER_", args.dataTier[0])
+    plotParamsD2[0] = plotParamsD2[0].replace("_DATATIER_", args.dataTier[1])
+    plotParamsD2[1] = plotParamsD2[1].replace("_DATATIER_", args.dataTier[1])
 
     graphs = TMultiGraph()
     gNameList = []
@@ -558,9 +593,11 @@ def plot2D_graph(filelist, args):
                     cutStrD1 = getCuts(args.vars[0], ch, dataTier1)
                     cutStrD2 = getCuts(args.vars[1], ch, dataTier2)
                     cutStr = "(" + cutStrD1 + ") && (" + cutStrD2 + ")"
-                cutStr = cutStr.replace("CHANNEL", ch)
                 if args.plotEach == "DM":
                     cutStr += " && Z_dm==" + str(dmFromName[hName])
+                if args.addCuts:
+                    cutStr += " && " + args.addCuts
+                cutStr = cutStr.replace("CHANNEL", ch)
 
 ## ------------------------------------------------------------------------------------------------------------------------------------------------- ##
 
