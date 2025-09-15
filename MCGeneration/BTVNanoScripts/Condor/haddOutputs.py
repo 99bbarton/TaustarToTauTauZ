cb51c1247481232abce78382157b13b43d46556a
BenBarton <bbarton@ucdavis.edu>
2025-09-15 17:40:29 -0500

    V1 processing of Run2+3 updates to nanoAOD tools and condor submission and processing infrastructure

--- MCGeneration/BTVNanoScripts/Condor/haddOutputs.py
+++ MCGeneration/BTVNanoScripts/Condor/haddOutputs.py
@@ -2,17 +2,9 @@
 import subprocess
 import argparse 
 
-processes =  ["ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST", "QCD"]
-sub_processes = {
-    "ZZ" : ["ZZto2L2Nu", "ZZto2L2Q", "ZZto2Nu2Q", "ZZto4L"],
-    "WZ" : ["WZto2L2Q", "WZto3LNu", "WZtoLNu2Q"],
-    "WW" : ["WWto2L2Nu", "WWto4Q", "WWtoLNu2Q"],
-    "WJets" : ["WtoLNu-4Jets"],
-    "DY" : ["DYto2L-2Jets_MLL-10to50", "DYto2L-2Jets_MLL-50"],
-    "TT" : ["TTto2L2Nu", "TTto4Q", "TTtoLNu2Q"], 
-    "ST" : ["TBbarQ_t-channel_4FS", "TWminusto2L2Nu", "TbarBQ_t-channel_4FS", "TbarWplusto2L2Nu", "TbarWplustoLNu2Q"],
-    "QCD" : [],
-}
+import sys
+sys.path.append("../../../Framework/")
+from datasets import processes, procToSubProc_run2, procToSubProc_run3, procToSubProc_run3_legacy
 
 #-------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -20,37 +12,62 @@ def haddFiles():
     argparser = argparse.ArgumentParser(description="Tool to hadd the outputs of the Condor jobs together")
     argparser.add_argument("-d", "--date", required=True, type=str, help="The date in the form e.g. 1Apr2025 that the jobs were created on")
     argparser.add_argument("-y", "--year", required=True, choices=["2016", "2016post", "2017", "2018", "2022", "2022post", "2023", "2023post"], help="What year the jobs correspond to")
-    argparser.add_argument("-p", "--processes", required=True, nargs="+", choices=["ALL", "ALLnoQCD", "ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST", "QCD"], help="Which samples to process")
+    argparser.add_argument("-p", "--processes", required=True, nargs="+", choices=["ALL", "SIG", "BKGD", "BKDGDnoQCD", "ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST", "QCD", "M250","M500","M750","M1000","M1500","M2000","M2500","M3000","M3500","M4000","M4500","M5000"], help="Which samples to process")
     argparser.add_argument("-v", "--version", required=True, type=str, help="A unique string to denote the new file area on EOS.")
+    argparser.add_argument("-l", "--legacy", required=False, action="store_true", help="If specified, uses old version of procToSubProc for Run3")
     args = argparser.parse_args()
 
-    if "ALL" in args.processes:
+    if "BKGD" in args.processes:
         args.processes = ["ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST", "QCD"]
-    elif "ALLnoQCD" in args.processes:
+        inDirBase = "/store/user/bbarton/TaustarToTauTauZ/BackgroundMC/PFNano/JobOutputs/" + args.date + "/"
+        outDirBase = "/store/user/bbarton/TaustarToTauTauZ//BackgroundMC/PFNano/"
+    elif "BKGDnoQCD" in args.processes:
         args.processes = ["ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST"]
-
-    IN_DIR_BASE = "/store/user/bbarton/TaustarToTauTauZ/BackgroundMC/PFNano/JobOutputs/" + args.date + "/"
-    OUT_DIR_BASE = "/store/user/bbarton/TaustarToTauTauZ//BackgroundMC/PFNano/"
-
+        inDirBase = "/store/user/bbarton/TaustarToTauTauZ/BackgroundMC/PFNano/JobOutputs/" + args.date + "/"
+        outDirBase = "/store/user/bbarton/TaustarToTauTauZ//BackgroundMC/PFNano/"
+    elif "SIG" in args.processes:
+        args.processes = ["M250","M500","M750","M1000","M1500","M2000","M2500","M3000","M3500","M4000","M4500","M5000"]
+        inDirBase = "/store/user/bbarton/TaustarToTauTauZ/SignalMC/SigPFNano/JobOutputs/" + args.date + "/"
+        outDirBase = "/store/user/bbarton/TaustarToTauTauZ/SignalMC/SigPFNano/"
     
-    inDir = IN_DIR_BASE + args.year + "/"
-    outDir = OUT_DIR_BASE + args.year + "/" + args.version + "/"
+    inDirBase = inDirBase + args.year + "/"
+    outDir = outDirBase + args.year + "/" + args.version + "/"
 
     print("Making output directory: " + outDir) 
     command  = "eosmkdir " + outDir
     stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
+
+    if args.year in ["2016", "2016post", "2017", "2018"]:
+        procToSubProc = procToSubProc_run2
+    elif args.year in ["2022", "2022post", "2023", "2023post"]:
+        if args.legacy:
+            procToSubProc = procToSubProc_run3_legacy
+        else:
+            procToSubProc = procToSubProc_run3
+    
+    
     for proc in args.processes:
-        inDir = IN_DIR_BASE + args.year + "/"+ proc + "/"
+        inDir = inDirBase + proc + "/"
 
         print("Starting " + args.year + " " + proc +  " samples...")
-        for subproc in sub_processes[proc]:
-            print("\thadd'ing " + subproc + " samples")
-            command = "hadd -f9 root://cmseos.fnal.gov/" + outDir + subproc + "_" + args.year + ".root `xrdfsls -u " + inDir + " | grep " + subproc + "`"
+        
+        if proc.startswith("M"):
+            print("\thadd'ing " + proc + " samples")
+            command = "hadd -f9 root://cmseos.fnal.gov/" + outDir + "/taustarToTauZ_" + proc.lower() + "_" + args.year + ".root `xrdfsls -u " + inDir + " | grep " + proc.lower() + "`"
             stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
             print(stdout)
             if len(stderr) > 0:
                 print("STDERR ", stderr)
+        else:
+            for subproc in procToSubProc[proc]:
+                print("\thadd'ing " + subproc + " samples")
+                command = "hadd -f9 root://cmseos.fnal.gov/" + outDir + subproc + "_" + args.year + ".root `xrdfsls -u " + inDir + " | grep " + subproc + "`"
+                stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+                print(stdout)
+                if len(stderr) > 0:
+                    print("STDERR ", stderr)
+            
 
 #-------------------------------------------------------------------------------------------------------------------------------------------
 
--- MCGeneration/BTVNanoScripts/Condor/makeScripts.py
+++ MCGeneration/BTVNanoScripts/Condor/makeScripts.py
@@ -6,32 +6,44 @@ import subprocess
 import copy
 
 sys.path.append("../../../Framework/")
-from datasets import bkgdDatasets_mini as miniDatasets
+from datasets import bkgdDatasets_mini, sigDatasets_mini
 
 #-------------------------------------------------------------------------------------------------------------------------------------------
 
 def parseArgs():
     argparser = argparse.ArgumentParser(description="Tool to make the .sh and .jdl scripts necessary to add PF info to background MC samples and then process them with nanoAOD-tools")
-    argparser.add_argument("-p", "--processes", required=True, nargs="+", choices=["ALL", "ALLnoQCD", "ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST", "QCD"], help="Which samples to process")
-    argparser.add_argument("-y", "--years", required=True, nargs="+", choices=["RUN3", "2022", "2022post", "2023", "2023post"], help="Which years to process")
+    argparser.add_argument("-p", "--processes", required=True, nargs="+", choices=["ALL", "SIG", "BKGD", "BKDGDnoQCD", "ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST", "QCD", "M250","M500","M750","M1000","M1500","M2000","M2500","M3000","M3500","M4000","M4500","M5000"], help="Which samples to process")
+    argparser.add_argument("-y", "--years", required=True, nargs="+", choices=["ALL", "RUN2", "RUN3", "2016", "2016post", "2017", "2018", "2022", "2022post", "2023", "2023post"], help="Which years to process")
     argparser.add_argument("-f", "--filesPerJob", required=False, type=int, default=10, help="The number of miniAOD dataset files to process per Condor job")
     argparser.add_argument("--justCount", action="store_true", help="If specified, will just print the number of jobs for each dataset and won't actually make configs")
     args = argparser.parse_args()
 
     if "ALL" in args.processes:
+        args.processes = ["ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST", "QCD", "M250", "M500", "M750", "M1000", "M1500", "M2000", "M2500", "M3000", "M3500", "M4000", "M4500", "M5000"]
+        hasSB = (True, True)
+    elif "BKGD" in args.processes:
         args.processes = ["ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST", "QCD"]
-    elif "ALLnoQCD" in args.processes:
+        hasSB = (False, True)
+    elif "BKGDnoQCD" in args.processes:
         args.processes = ["ZZ", "WZ", "WW", "WJets", "DY", "TT", "ST"]
+        hasSB = (False, True)
+    elif "SIG" in args.processes:
+        args.processes = ["M250","M500","M750","M1000","M1500","M2000","M2500","M3000","M3500","M4000","M4500","M5000"]
+        hasSB = (True, False)
 
-    if "RUN3" in args.years:
+    if "ALL" in args.years:
+        args.years = ["2016", "2016post", "2017", "2018", "2022", "2022post", "2023", "2023post"]
+    elif "RUN3" in args.years:
         args.years = ["2022", "2022post", "2023", "2023post"]
+    elif "RUN2" in args.years:
+        args.years = ["2016", "2016post", "2017", "2018"]
     
-    return args
+    return args, hasSB
 
 #-------------------------------------------------------------------------------------------------------------------------------------------
 
 def main(args):
-    args = parseArgs()
+    args, hasSB = parseArgs()
 
     months = ["","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
     tod = date.today()
@@ -40,43 +52,65 @@ def main(args):
         logFile.write("Creating job configs and excecution scripts in directory JobSubmissions" + dateStr + "/" + str(args.years) + "/\n")
         logFile.write("parseArgs returned the following parameters: " + str(args) + "\n")
     
-    makeScripts(args, dateStr)
+    makeScripts(args, dateStr, hasSB)
         #logFile.write("The cmsDriver command of the last job was: \n")
         #logFile.write(command + "\n\n")
 
 #-------------------------------------------------------------------------------------------------------------------------------------------
 
-def makeScripts(args, dateStr):
+def makeScripts(args, dateStr, hasSB):
     dirsToMake = []
     
     for year in args.years:
+        if year in ["2016", "2016post", "2017", "2018"]:
+            era = 2
+        else:
+            era = 3
+
+        
         print("Making configuration files for " + year)
-        outDirBase = "/store/user/bbarton/TaustarToTauTauZ/BackgroundMC/PFNano/JobOutputs/" + dateStr + "/" + year + "/"
 
         if not args.justCount:
-            command = "eosmkdir /store/user/bbarton/TaustarToTauTauZ/BackgroundMC/PFNano/JobOutputs/" + dateStr
-            stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-            command = "eosmkdir " + outDirBase
-            stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+            if hasSB[0]:
+                command = "eosmkdir /store/user/bbarton/TaustarToTauTauZ/SignalMC/SigPFNano/JobOutputs/" + dateStr
+                stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+                command = command + "/" + year + "/"
+                stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+            if hasSB[1]:
+                command = "eosmkdir /store/user/bbarton/TaustarToTauTauZ/BackgroundMC/PFNano/JobOutputs/" + dateStr
+                stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+                command = command + "/" + year + "/"
+                stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+            
             
         
         scriptDir = "Jobs/" + dateStr + "/" + year + "/" 
 
         os.system("mkdir Jobs/" + dateStr)
         os.system("mkdir Jobs/" + dateStr + "/" + year)
-        
-        cmssw_pfNano = "CMSSW_13_0_13"
-        cmssw_nano = "CMSSW_14_1_1"
+
+        if era == 3:
+            cmssw_pfNano = "CMSSW_13_0_13"
+            cmssw_nano = "CMSSW_14_1_1"
+        else: 
+            cmssw_pfNano = "CMSSW_10_6_29"
+            cmssw_nano = "CMSSW_14_1_1"
 
         for proc in args.processes:
-            outDir = outDirBase + proc + "/"
-            #dirsToMake.append(outDir)
-            if not args.justCount:
+            if proc.startswith("M"):
+                outDir = "/store/user/bbarton/TaustarToTauTauZ/SignalMC/SigPFNano/JobOutputs/" + dateStr + "/" + year + "/"
+            else:
+                outDir = "/store/user/bbarton/TaustarToTauTauZ/BackgroundMC/PFNano/JobOutputs/" + dateStr + "/" + year + "/" + proc + "/"
+            
+            if not args.justCount and not proc.startswith("M"):
                 print("Making directory: " + outDir)
                 command = "eosmkdir " + outDir
                 stdout, stderr  = subprocess.Popen(command, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-            
-            dataSets = miniDatasets[year][proc]
+
+            if proc.startswith("M"):
+                dataSets = [sigDatasets_mini[year][proc]]
+            else:
+                dataSets = bkgdDatasets_mini[year][proc]
             for dataset in dataSets:
                 dasCommand = 'dasgoclient --query="file dataset=' + dataset + '"'
                 stdout, stderr  = subprocess.Popen(dasCommand, universal_newlines=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
@@ -112,7 +146,10 @@ def makeScripts(args, dateStr):
                         executable.write("source /cvmfs/cms.cern.ch/cmsset_default.sh\n")
                         executable.write("tar -xf " + cmssw_pfNano + ".tgz\n")
                         executable.write("rm " + cmssw_pfNano + ".tgz\n")
-                        executable.write("cd " + cmssw_pfNano + "/src/btvnano-prod/\n")
+                        if era == 2:
+                            executable.write("cd " + cmssw_pfNano + "/src/PhysicsTools/PFNano/\n")
+                        else:
+                            executable.write("cd " + cmssw_pfNano + "/src/btvnano-prod/\n")
                         executable.write("scramv1 b ProjectRename # this handles linking the already compiled code - do NOT recompile\n")
                         executable.write("eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers\n")
 
@@ -136,7 +173,10 @@ def makeScripts(args, dateStr):
                         executable.write("scramv1 b ProjectRename # this handles linking the already compiled code - do NOT recompile\n")
                         executable.write("eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers\n")
                         executable.write("cd PhysicsTools/NanoAODTools/condor/\n")
-                        executable.write("mv $HOME/" + cmssw_pfNano + "/src/btvnano-prod/" + subDataset +"*.root .\n")
+                        if era == 2: 
+                            executable.write("mv $HOME/" + cmssw_pfNano + "/src/PhysicsTools/PFNano/" + subDataset +"*.root .\n")
+                        else:
+                            executable.write("mv $HOME/" + cmssw_pfNano + "/src/btvnano-prod/" + subDataset +"*.root .\n")
                         executable.write("rm -r $HOME/" + cmssw_pfNano + "\n")
                         #Perform the NanoAOD-tools processing of the new custom nano+PF files
                         executable.write("python3 condorScript.py " + year + "\n")
@@ -160,6 +200,8 @@ def makeScripts(args, dateStr):
                         jdlFile.write('Error = condor_PFNano-Nano_$(Cluster)_$(Process).stderr\n')
                         jdlFile.write('Log = condor_PFNano-Nano_$(Cluster)_$(Process).log\n')
                         jdlFile.write('Queue 1\n')
+                        if era == 2:
+                            jdlFile.write('+DesiredOS = "SL7"')
                     #End jdl creation
                 #End jobN
             #End dataset
