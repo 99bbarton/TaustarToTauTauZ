49b094f57882737d001b11114ac7450882df2dbf
BenBarton <bbarton@ucdavis.edu>
2026-02-17 17:01:39 -0600

    trivial fix

--- Combine/runCombine.py
+++ Combine/runCombine.py
@@ -11,7 +11,7 @@ import argparse
 def parseArgs():
     argparser = argparse.ArgumentParser(description="Utility to script to run Combine")
     argparser.add_argument("-i", "--inDir", default="./Datacards/", help="A path to the directory containing datacards")
-    argparser.add_argument("-f", "--files", nargs="+", help="A specific datacard file to run over. It should be in the path specified by --inDir")
+    argparser.add_argument("-f", "--files", nargs="+", default=[],help="A specific datacard file to run over. It should be in the path specified by --inDir")
     argparser.add_argument("-l", "--limit", action="store_true", help="If specified, runs AsymptoticLimits method")
     argparser.add_argument("-t", "--toys", type=int, default=100, help="The number of toys to use")
     argparser.add_argument("-n", "--nameTag", type=str, default="", help="A specific naming tag (will be present in output .root file)")
