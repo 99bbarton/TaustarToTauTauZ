6c96833070c129a3adb9c73d4fe5d8e7b22fe00a
BenBarton <bbarton@ucdavis.edu>
2026-04-07 17:56:30 -0500

    Bug fixes to PDF weight calculation script and add paths to link against LHADPDF library for run3 CMSSWs

--- Analysis/addPDFWeights.C
+++ Analysis/addPDFWeights.C
@@ -5,15 +5,28 @@
 /*
   This macro requires both ROOT and the LHAPDF library so must be compiled against both.
   To compile using ROOT on LPC:
+
+run after settign up environment:  lhapdf-config --cflags --ldflags     to get the paths necessary for compilation
+      and linking against LHAPDF. The paths for run2 and run3 examples are included below
+
+
+For Run 2:
+  - sing
   - Set up a CMS env (>= CMSSW_10_6_X )
-  - run:  lhapdf-config --cflags --ldflags     to get the paths necessary for compilation
-      and linking against LHAPDF
   - start root:    root
   - in root, run :  gSystem->AddIncludePath(" -I/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/lhapdf/6.2.1-pafccj3/include ")     where the path matches the include path from the lhapdf-config
   - in root, run : gSystem->AddLinkedLibs("-L/cvmfs/cms.cern.ch/slc7_amd64_gcc820/external/lhapdf/6.2.1-pafccj3/lib -lLHAPDF")   where the path matches the include path from the lhapdf-config
   - in root, compile this script:    .L pdfWeightAdder.C+
   - The functions below can now be run
 
+
+For Run 3:
+  - Set up a CMS env (e.g. CMSSW_14_1_1)
+  - root
+  - >  gSystem->AddIncludePath(" -I/cvmfs/cms.cern.ch/el9_amd64_gcc12/external/lhapdf/6.4.0-4aa384870ea6380250957dc033c78953/include ")
+  - >  gSystem->AddLinkedLibs("-L/cvmfs/cms.cern.ch/el9_amd64_gcc12/external/lhapdf/6.4.0-4aa384870ea6380250957dc033c78953/lib -lLHAPDF")
+  - >  .L pdfWeightAdder.C+
+  - >  pdfWeightAdder("<YEAR>")
 */
 
 #include "LHAPDF/LHAPDF.h"
@@ -35,29 +48,29 @@ double calcFactorizWeight(LHAPDF::PDF* pdf, double id1, double id2, double x1, d
 void pdfWeightAdder(TString year)
 {   
 
-    int nomPDF;
+    int nomPDFN;
     int nVars;
-    if year == "2022" || year == "2022post" || year == "2023" || year == "2023post" || year == "2024";
-        nomPDF = 325500;  
+    if (year == "2022" || year == "2022post" || year == "2023" || year == "2023post" || year == "2024")
+        nomPDFN = 325500;  
     else
-        nomPDF = 303600;
+        nomPDFN = 303600;
 
     //Get the PDFs
-    PDF* nomPDF = LHAPDF::mkPDF(nomPDF); 
-    PDF* varPDFs[nVars];
-    for (int i = 0; i< nVars; i++)
+    PDF* nomPDF = LHAPDF::mkPDF(nomPDFN); 
+    PDF* varPDFs[100];
+    for (int i = 0; i< 100; i++)
     {
-        varPDFs[i] = LHAPDF::mkPDF(nomPDF + 1 + i);
+        varPDFs[i] = LHAPDF::mkPDF(nomPDFN + 1 + i);
     }
     
     //Add the weights to each file
     cout << "\nBeginning " << year << "..." << endl;
-    TString masses[N_FILES] = {"m250","m500","m750","m1000","m1250","m1500","m1750","m2000","m2500","m3000","m3500","m4000","m4500","m5000"};
+    TString masses[14] = {"m250","m500","m750","m1000","m1250","m1500","m1750","m2000","m2500","m3000","m3500","m4000","m4500","m5000"};
 
-    for (int fN = 0; fN < N_FILES; fN++)
+    for (int fN = 0; fN < 14; fN++)
     {
         //TString filename = "root://cmsxrootd.fnal.gov//store/user/bbarton/TaustarToTauTauZ/SignalMC/SigPFNano/"+year+"/V2/taustarToTauZ_"+masses[fN]+"_"+year+".root";
-        TString filename = "../Data/SignalMC/WithPDFWeights/"+year+"/V2/taustarToTauZ_"+masses[fN]+"_"+year+".root";
+        TString filename = "../Data/SignalMC/WithPDFWeights/"+year+"/taustarToTauZ_"+masses[fN]+"_"+year+".root";
         cout << "\tAdding weights to file:  " << filename << endl;
         addPDFWeights(filename, 0, nomPDF, varPDFs);
     }
@@ -89,7 +102,7 @@ void addPDFWeights(TString filename, int nQCD, PDF* nomPDF, PDF* varPDFs[])
     TBranch *b_alphas = tree->Branch("PDFWeights_alphas", &alphas, "PDFWeights_alphas/D");
     TBranch *b_renormWeights = tree->Branch("PDFWeights_renormWeights", renormWeights, "PDFWeights_renormWeights[2]/D");
     //TBranch *b_nVarsUD = tree->Branch("PDFWeights_nVarsUD", &nVarsUD, "PDFWeights_nVarsUD/i");
-    TBranch *b_nVars = tree->Branch("PDFWeights_nVars", &nVars, "PDFWeights_nVars/i");
+    TBranch *b_nVars = tree->Branch("PDFWeights_nVars", 100, "PDFWeights_nVars/i");
     //TBranch *b_factorizeWeights = tree->Branch("PDFWeights_factorizeWeights", factorizWeights, "PDFWeights_factorizeWeights[200]/D");
     //TBranch *b_weightsForVar = tree->Branch("PDFWeights_weightsForVar", weightsForVar, "PDFWeights_weightsForVar[100]/D");
     TBranch *b_factWeightsRMSs = tree->Branch("PDFWeights_factWeightsRMSs", factWeightsRMSs, "PDFWeights_factWeightsRMSs[2]/D");
@@ -133,11 +146,11 @@ void addPDFWeights(TString filename, int nQCD, PDF* nomPDF, PDF* varPDFs[])
         }
 
         //Calculate the RMS's
-        factWeightsRMSs[0] /= nVars;
-        factWeightsRMSs[1] /= nVars; 
+        factWeightsRMSs[0] /= 100;
+        factWeightsRMSs[1] /= 100; 
         factWeightsRMSs[0] = sqrt(factWeightsRMSs[0]);
         factWeightsRMSs[1] = sqrt(factWeightsRMSs[1]);
-        varWeightsRMS /= nVars;
+        varWeightsRMS /= 100;
         varWeightsRMS = sqrt(varWeightsRMS);
 
 
